715c5bce05f4cf878fe40c7e0631412ae2cc6e81
ksagar03 <sagar22krishna03@gmail.com>
2024-04-10 20:24:18 +0530

    minor update: for smaller screen moved "explore anime" to the centre

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ const Home = async () => {
     <>
       <Header />
       <main className="sm-p-16 px-8 flex flex-col gap-10">
-        <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
+        <h2 className="text-3xl md:text-center mt-1 text-white font-bold">Explore Anime</h2>
         <section className="grid grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-10">
           {data}
         </section>
